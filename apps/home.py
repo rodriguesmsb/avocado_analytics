f53b293f0a4ed7644dece7a17456fec0286b0f5d
rodriguemsb <rodriguesmsb@gmail.com>
2021-01-12 15:45:29 -0400

    start a mult page app

---
+++ apps/home.py
@@ -0,0 +1,46 @@
+import dash_core_components as dcc  
+import dash_html_components as html
+import base64
+
+
+path_to_image = "assets/heart.png"
+def encode_image(image_file):
+    ''' 
+    Function to encode a image in a format that allows its plot on html.Fig
+    '''
+    encode = base64.b64encode(open(image_file, "rb").read())
+    return "data:image/jpeg;base64,{}".format(encode.decode())
+
+
+layout = (
+    html.Div(
+        children = [
+            #   Create a div element to hold header
+            html.Div(
+                children = [
+                    html.P(children = "🥑", className = "emoji"),
+                    html.H1(children = "Avocado Analytics",
+                    className = "header-title"),
+                    html.P(
+                    children = "A web page dedicated to analyze"
+                    " the behavior of avocado prices in US"
+                    " between 2015 and 2018",
+                    className = "description"
+                    ),
+                    
+            ],
+            className = "header"
+        ),
+            html.Div(children = [
+                html.P("Because we all love AVOCADOS!", className = "description"),
+                html.Img(src = encode_image(path_to_image))
+            
+        ],
+            className = "footer"
+
+
+    ),
+    ],
+
+    )
+)
---
+++ index.py
@@ -0,0 +1,40 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+from app import app
+from apps import home
+
+###Add code to use external css
+external_stylesheets = [
+    {
+        "href": "https://fonts.googleapis.com/css2?"
+                "family=Lato:wght@400;700&display=swap",
+        "rel": "stylesheet",
+    },
+]
+
+
+###Create a instance of Dash class
+app = dash.Dash(__name__, external_stylesheets = external_stylesheets)
+app.title = "Avocado Analytics: Understand Your Avocados!"
+
+app.layout = html.Div([
+    dcc.Location(id = 'url', refresh = False),
+    html.Div(id = 'page-content')
+])
+
+
+@app.callback(Output('page-content', 'children'),
+
+              Input('url', 'pathname'))
+def display_page(pathname):
+    if pathname == '/apps/app1':
+        pass
+    else:
+        return home.layout
+
+
+if __name__ == '__main__':
+    app.run_server(debug = True)
