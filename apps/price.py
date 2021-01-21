a9a96fe41f058045720cb1c3fbf28abd42064cf7
rodriguemsb <rodriguesmsb@gmail.com>
2021-01-21 10:39:23 -0400

    add menu sel

--- apps/price.py
+++ apps/price.py
@@ -1,11 +1,17 @@
 import dash_core_components as dcc  
 import dash_html_components as html
+import pandas as pd
+import numpy as np
 
 from components import nav_bar
 from components.aux_func import encode_image
 
 path_to_image = "assets/heart.png"
 path_to_avocado = "assets/favicon.ico"
+
+data = pd.read_csv("data/avocado.csv")
+data["Date"] = pd.to_datetime(data["Date"], format="%Y-%m-%d")
+data.sort_values("Date", inplace=True)
 
 
 layout = html.Div(
@@ -30,7 +36,61 @@ layout = html.Div(
         #Add a div with navbar over here
         nav_bar.navBar,
 
+        #Add menu selector
+        html.Div(
+            children=[
+                html.Div(
+                    children=[
+                        html.Div(children="Region", className="menu-title"),
+                        dcc.Dropdown(
+                            id="region-filter",
+                            options=[
+                                {"label": region, "value": region}
+                                for region in np.sort(data.region.unique())
+                            ],
+                            value="Albany",
+                            clearable=False,
+                            className="dropdown",
+                        ),
+                    ]
+                ),
+                html.Div(
+                    children=[
+                        html.Div(children = "Type", className = "menu-title"),
+                        dcc.Dropdown(
+                            id="type-filter",
+                            options=[
+                                {"label": avocado_type, "value": avocado_type}
+                                for avocado_type in data.type.unique()
+                            ],
+                            value = "organic",
+                            clearable = False,
+                            searchable = False,
+                            className="dropdown",
+                        ),
+                    ],
+                ),
+                html.Div(
+                    children=[
+                        html.Div(
+                            children="Date Range",
+                            className="menu-title"
+                            ),
+                        dcc.DatePickerRange(
+                            id="date-range",
+                            min_date_allowed=data.Date.min().date(),
+                            max_date_allowed=data.Date.max().date(),
+                            start_date=data.Date.min().date(),
+                            end_date=data.Date.max().date(),
+                        ),
+                    ]
+                ),
+            ],
+            className="menu",
+        ),
+
         #Add graph here
+
     
         html.Div(id = "footer_body",
                 children = [
